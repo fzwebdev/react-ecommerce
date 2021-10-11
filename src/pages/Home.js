05bd17a5ab328c55efd1178c4f7fe3095ca588c2
Firoz Ansari <fzwebdev00@gmail.com>
2021-10-11 10:01:34 +0530

    category design added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Anouncement from '../components/Anouncement'
+import Categories from '../components/Categories';
 import Navbar from '../components/Navbar'
 import Slider from '../components/Slider';
 
@@ -9,6 +10,7 @@ const Home = () => {
             <Anouncement />
             <Navbar />
             <Slider />
+            <Categories />
         </>
     )
 }
